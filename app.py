2cd8811a323d31fa55e6dc09565d7b005f37ec70
krishnakumarradhakrishnan <krishnakumar.radhakrishnan_ext@appviewx.com>
2024-05-22 15:51:59 +0530

    changed in namea

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-data = {"incomes": [{"name":"Krishna","rollno":2001115}]}
+data = {"Students": [{"name":"Krishna","rollno":2001115}]}
 
-@app.route('/incomes', methods=['POST'])
-def incomes_post():
+@app.route('/Students', methods=['POST'])
+def Students_post():
     new_income = request.get_json()
-    data["incomes"].append(new_income)
+    data["Students"].append(new_income)
     with open('output.json', 'w') as f:
         json.dump(data, f)
     return jsonify({"message": "Income added successfully"}), 201
 
-@app.route('/incomes/<int:index>', methods=['GET','PUT','DELETE'])
-def incomes(index):
+@app.route('/Students/<int:index>', methods=['GET','PUT','DELETE'])
+def Students(index):
 
     if request.method == 'GET':
         with open('output.json', 'r') as f:
             loaded_data = json.load(f)
 
-            if 0 <= index < len(loaded_data["incomes"]):
-                name = loaded_data["incomes"][index]["name"] 
-                rollno = loaded_data["incomes"][index]["rollno"]
+            if 0 <= index < len(loaded_data["Students"]):
+                name = loaded_data["Students"][index]["name"] 
+                rollno = loaded_data["Students"][index]["rollno"]
             else:
                 name = "world"
                 rollno="NA"
@@ -32,9 +32,9 @@ def incomes(index):
     if request.method == 'PUT':
         with open('output.json', 'r') as f:
             loaded_data = json.load(f)
-            if 0 <= index < len(loaded_data["incomes"]):
+            if 0 <= index < len(loaded_data["Students"]):
                 updated_income = request.get_json()
-                loaded_data["incomes"][index] = updated_income
+                loaded_data["Students"][index] = updated_income
                 with open('output.json', 'w') as f:
                     json.dump(loaded_data, f)
                 return jsonify({"message": "Income updated successfully"}), 200
@@ -44,8 +44,8 @@ def incomes(index):
     if request.method == 'DELETE':
         with open('output.json', 'r') as f:
             loaded_data = json.load(f)
-            if 0 <= index < len(loaded_data["incomes"]):
-                del loaded_data["incomes"][index]
+            if 0 <= index < len(loaded_data["Students"]):
+                del loaded_data["Students"][index]
                 with open('output.json', 'w') as f:
                     json.dump(loaded_data, f)
                 return jsonify({"message": "Income deleted successfully"}), 200
